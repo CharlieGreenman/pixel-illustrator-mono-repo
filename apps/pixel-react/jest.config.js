abb98596e92e97f2d0ab8f9977b6228ff79e1a2b
Charlie Greenman <charie@razroo.com>
2019-04-07 19:09:06 +0300

    Create a pixel-react application.

---
+++ apps/pixel-react/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'pixel-react',
+  preset: '../../jest.config.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  coverageDirectory: '../../coverage/apps/pixel-react/'
+};
