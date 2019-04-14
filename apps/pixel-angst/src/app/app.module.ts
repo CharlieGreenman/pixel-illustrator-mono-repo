448170d986e78e8416b8be27206e00dfa195323e
Charlie Greenman <charie@razroo.com>
2019-04-14 20:21:23 +0300

    Hook up the choose-size component into the app component html.

--- apps/pixel-angst/src/app/app.module.ts
+++ apps/pixel-angst/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PixelAngstCommonUiModule } from '@pxl/pixel-angst/common/ui';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, PixelAngstCommonUiModule],
   providers: [],
   bootstrap: [AppComponent]
 })
