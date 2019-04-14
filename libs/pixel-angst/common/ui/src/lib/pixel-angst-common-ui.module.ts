93b8f19123557daa7f27ff2f60b0eae6ef8f03ae
Charlie Greenman <charie@razroo.com>
2019-04-14 20:49:04 +0300

    Update package.json to the latest. Add angular material and angular form into the pixel-angst common ui module.

--- libs/pixel-angst/common/ui/src/lib/pixel-angst-common-ui.module.ts
+++ libs/pixel-angst/common/ui/src/lib/pixel-angst-common-ui.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChooseSizeComponent } from './choose-size/choose-size.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatFormFieldModule } from '@angular/material';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, MatFormFieldModule],
   declarations: [ChooseSizeComponent],
   exports: [ChooseSizeComponent],
 })
